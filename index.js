a42176d66450e5a929dfafa1b892ca43f6686178
ilteris <ilteris@gmail.com>
2014-01-18 16:23:42 -0500

    add homepage route

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var express = require("express");
 var http = require("http");
 var app = express();
 
+app.get("/", function(req, res, next) {
+    res.send("Hello <strong> home page </strong>");
+});
 
